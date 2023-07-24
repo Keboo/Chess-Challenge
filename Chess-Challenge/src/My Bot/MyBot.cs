9a3b38207b70857af55dd100488935b03d53be89
Kevin Bost <kitokeboo@gmail.com>
2023-07-23 23:17:00 -0700

    Simple eval of material value

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,5 @@
 ﻿using ChessChallenge.API;
+using Raylib_cs;
 using System;
 using System.Linq;
 
@@ -9,18 +10,31 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        Console.WriteLine("---");
+        
         //byte[] bytes = "Foo".SelectMany(x => new[] { (byte)(x >> 8 & 0xFF), (byte)(x & 0xFF) }).ToArray()
         return (from move in board.GetLegalMoves()
                let rating = RateMove(board, move)
                orderby rating descending, random.Next()
                select move).First();
+
     }
 
-    //Return a value -100 to 100 for how good this move is
     private static double? RateMove(Board board, Move move)
     {
-
+        int before = EvalMaterial(board);
         board.MakeMove(move);
+        int after = EvalMaterial(board);
+        board.UndoMove(move);
+        if (before == after)
+        {
+            return 0;
+        }
+        if (before == 0)
+        {
+            return after;
+        }
+        return after / before;
         double value = 0;
 
         try
@@ -50,7 +64,50 @@ public class MyBot : IChessBot
         {
             board.UndoMove(move);
         }
+        
 
+        if (board.TrySkipTurn())
+        {
+            try
+            {
+                Move[] nextMoves = board.GetLegalMoves();
+                int movedPieceValue = pieceValues[(int)move.MovePieceType];
+
+                if (move.IsCapture)
+                {
+                    value += pieceValues[(int)move.MovePieceType];
+                }
+            }
+            finally
+            {
+                board.UndoSkipTurn();
+            }
+        }
+
+        Console.WriteLine($"Move {move} => {value}");
         return value;
+    }
+
+    private static int EvalMaterial(Board board)
+    {
+        var allList = board.GetAllPieceLists();
+        int myMaterial, theirMaterial;
+        if (board.IsWhiteToMove)
+        {
+            myMaterial = allList[0..6].Select(ValueList).Sum();
+            theirMaterial = allList[6..].Select(ValueList).Sum();
+        }
+        else
+        {
+            myMaterial = allList[6..].Select(ValueList).Sum();
+            theirMaterial = allList[0..6].Select(ValueList).Sum();
+        }
+
+        return myMaterial - theirMaterial;
+
+        static int ValueList(PieceList list)
+        {
+            return pieceValues[(int)list.TypeOfPieceInList] * list.Count;
+        }
     }
 }
